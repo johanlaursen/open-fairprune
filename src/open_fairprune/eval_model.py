3e9ba5ddf7521d161058b642c6cc27b8e6b08dc3
christoffer <csom@itu.dk>
2023-05-02 13:48:32 +0200

    add fairprune metrics

--- src/open_fairprune/eval_model.py
+++ src/open_fairprune/eval_model.py
@@ -13,6 +13,31 @@ from open_fairprune.data_util import get_dataset, load_model
 from open_fairprune.train import metric
 
 
+class GroupScore(typing.NamedTuple):
+    total: float
+    group0: float
+    group1: float
+
+    def __repr__(self):
+        total, group0, group1 = self.total, self.group0, self.group1
+        return f"{total = :.2f} ({group0 = :.2f}, {group1 = :.2f})"
+
+
+class GeneralMetrics(typing.NamedTuple):
+    accuracy: GroupScore
+    f1: GroupScore
+    tpr: GroupScore  # recall, sensitivity
+    fpr: GroupScore  # 1 - specificity
+    tnr: GroupScore  # Specificity
+    roc_auc: GroupScore
+
+    def __repr__(self):
+        out = []
+        for field in self._fields:
+            out.append(f"{field:<10}: {getattr(self, field)}")
+        return "\n".join(out)
+
+
 class FairnessMetrics(typing.NamedTuple):
     Δ_parity: float
     Δ_eq_odds_t0: float
@@ -29,28 +54,23 @@ class FairnessMetrics(typing.NamedTuple):
         {self.Δ_eq_out_s1  = :.2f}"""
 
 
-class GroupScore(typing.NamedTuple):
-    total: float
-    group0: float
-    group1: float
+class FairPruneMetrics(typing.NamedTuple):
+    # Paper cites [8] https://papers.nips.cc/paper_files/paper/2016/file/9d2682367c3935defcb1f9e247a97c0d-Paper.pdf
+    EOpp0: float
+    EOpp1: float
+    EOdd: float
+
+    @classmethod
+    def from_general_metrics(cls, metrics: GeneralMetrics) -> "FairPruneMetrics":
+        # For EOpp: The paper sums this for y=1 and y=0, but in binary classification we can just do y=1?
+        EOpp0 = abs(metrics.tnr.group0 - metrics.tnr.group1)
+        EOpp1 = abs(metrics.tpr.group0 - metrics.fpr.group1)
+        EOdd = abs(metrics.tpr.group1 - metrics.tpr.group0 + metrics.fpr.group1 - metrics.fpr.group0)
+        return cls(EOpp0, EOpp1, EOdd)
 
     def __repr__(self):
-        total, group0, group1 = self.total, self.group0, self.group1
-        return f"{total = :.2f} ({group0 = :.2f}, {group1 = :.2f})"
-
-
-class GeneralMetrics(typing.NamedTuple):
-    accuracy: GroupScore
-    f1: GroupScore
-    tpr: GroupScore  # recall, sensitivity
-    fpr: GroupScore  # 1 - specificity
-    roc_auc: GroupScore
-
-    def __repr__(self):
-        out = []
-        for field in self._fields:
-            out.append(f"{field:<10}: {getattr(self, field)}")
-        return "\n".join(out)
+        EOpp0, EOpp1, EOdd = self.EOpp0, self.EOpp1, self.EOdd
+        return f"{EOpp0 = :.2f} {EOpp1 = :.2f} {EOdd = :.2f}"
 
 
 def get_fairness_metrics(y_pred, y_true, group, *, thresh=0.5, verbose=False) -> FairnessMetrics:
@@ -69,7 +89,7 @@ def get_fairness_metrics(y_pred, y_true, group, *, thresh=0.5, verbose=False) ->
         }
     )
     # If error, return nan
-    Δ_parity = Δ_eq_odds_t0 = Δ_eq_odds_t1 = Δ_eq_out_s0 = Δ_eq_out_s1 = np.nan
+    Δ_parity = Δ_eq_odds_t0 = Δ_eq_odds_t1 = Δ_eq_out_s0 = Δ_eq_out_s1 = Δ_eq_opp = np.nan
 
     pr_s_cond_g = df.groupby("G").S.mean()
     Δ_parity = abs(pr_s_cond_g.diff()[-1])
@@ -80,7 +100,9 @@ def get_fairness_metrics(y_pred, y_true, group, *, thresh=0.5, verbose=False) ->
         Δ_eq_odds_t0 = abs(pr_s_cond_gt.xs(False, level="T").diff()[-1])
     with suppress(KeyError):
         Δ_eq_odds_t1 = abs(pr_s_cond_gt.xs(True, level="T").diff()[-1])
-    pr_s_cond_gt
+
+    with suppress(KeyError):
+        Δ_eq_opp_s1 = abs(pr_s_cond_gt.xs(True, level="S").diff()[-1])
 
     pr_t_cond_gs = df.groupby(["G", "S"])["T"].mean()
     with suppress(KeyError):
@@ -115,6 +137,7 @@ def get_general_metrics(y_pred, y_true, group) -> FairnessMetrics:
         f1=F1Score("binary"),
         tpr=Recall("binary"),
         fpr=lambda x, y: 1 - Specificity("binary")(x, y),
+        tnr=Specificity("binary"),
         roc_auc=AUROC("binary"),
     )
 
@@ -143,7 +166,13 @@ if __name__ == "__main__":
         y_pred = model(data.to("cuda")).softmax(dim=1).detach().cpu()
 
     fairness_metrics = get_fairness_metrics(y_pred, y_true, group, thresh=0.5)
+    print()
     print(fairness_metrics)
 
     general_metrics = get_general_metrics(y_pred, y_true, group)
+    print()
     print(general_metrics)
+
+    fairprune_metrics = FairPruneMetrics.from_general_metrics(general_metrics)
+    print()
+    print(fairprune_metrics)
