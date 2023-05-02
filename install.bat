6f2d311fd12bc37f1e35e2991782c792b46911e6
christoffer <csom@itu.dk>
2023-05-02 07:48:48 +0200

    fix deps

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-set PIP_EXTRA_INDEX_URL=https://download.pytorch.org/whl/cu117
+set PIP_EXTRA_INDEX_URL=https://pypi.python.org/simple https://download.pytorch.org/whl/cu117
 
 python -m venv venv
 ./venv/Scripts/activate
