e70f3bdb035c9c26be8dc8455f32fd40f94d36ae
christoffer <csom@itu.dk>
2023-05-03 19:34:49 +0200

    set seed

--- src/open_fairprune/__init__.py
+++ src/open_fairprune/__init__.py
@@ -1,9 +1,16 @@
+import random
 from pathlib import Path
 
 import mlflow
+import numpy as np
+import torch
 
 import open_fairprune
 from open_fairprune.data_util import DATA_PATH
+
+random.seed(0)
+np.random.seed(0)
+torch.manual_seed(42)
 
 mlflow.set_tracking_uri(uri=f"file://{DATA_PATH}\mlruns")
 
