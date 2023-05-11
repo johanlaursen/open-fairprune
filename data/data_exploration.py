78354f841c713d48ebac6bc414dbd6b2dfeb06aa
christoffer <csom@itu.dk>
2023-05-11 10:25:56 +0200

    seperate datawig install

--- data/data_exploration.py
+++ data/data_exploration.py
@@ -3,17 +3,63 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.preprocessing import StandardScaler
 
-from open_fairprune.data_util import FLOAT_COLUMNS, get_split_df
+FLOAT_COLUMNS = [
+    "Active_Loan",
+    "Age_Days",
+    "Application_Process_Day",
+    "Application_Process_Hour",
+    "Bike_Owned",
+    "Car_Owned",
+    "Child_Count",
+    "Cleint_City_Rating",
+    "Client_Family_Members",
+    "Client_Income",
+    "Credit_Amount",
+    "Credit_Bureau",
+    "Default",
+    "Employed_Days",
+    "Homephone_Tag",
+    "House_Own",
+    "ID",
+    "ID_Days",
+    "Loan_Annuity",
+    "Mobile_Tag",
+    "Own_House_Age",
+    "Phone_Change",
+    "Population_Region_Relative",
+    "Registration_Days",
+    "Score_Source_1",
+    "Score_Source_2",
+    "Score_Source_3",
+    "Social_Circle_Default",
+    "Workphone_Working",
+]
 
 
 def main():
-    train_df = get_split_df("train")
+    df = pd.read_csv("Train_Dataset.csv")
+    splits = {
+        "train": df.ID % 7 <= 4,
+        "dev": df.ID % 7 == 5,
+        "test": df.ID % 7 == 6,  # Around 15%
+    }
+    df = df[splits["train"]]
+
+    def isfloat(x):
+        try:
+            float(x)
+            return True
+        except:
+            return False
+
+    df.loc[:, FLOAT_COLUMNS] = df[FLOAT_COLUMNS][df[FLOAT_COLUMNS].applymap(isfloat)]
+    train_df = df.astype({c: "float" for c in FLOAT_COLUMNS})
 
     # Null values in each column and percentage missing:
     print((train_df.isnull().sum() / len(train_df)).sort_values(ascending=False))
 
     # Dropping everything with more than 50% missing values and score source 3
-    train_df = train_df.dropna(thresh=len(train_df) / 2, axis=1)
+    # train_df = train_df.dropna(thresh=len(train_df) / 2, axis=1)
     # df = df.drop(['Score_Source_3'], axis=1)
 
     # Replacing nan values in Client Occupation with 'nan'
@@ -27,13 +73,14 @@ def main():
     X_train = train_df.drop(["Default"], axis=1)
 
     # Standardize float columns
-    X_train[FLOAT_COLUMNS] = StandardScaler().fit_transform(X_train[FLOAT_COLUMNS])
+    TRAIN_FLOAT = list(set(FLOAT_COLUMNS) - set(["Default"]))
+    X_train[TRAIN_FLOAT] = StandardScaler().fit_transform(X_train[TRAIN_FLOAT])
 
     # Write down mse to a file
     with open("mse.txt", "w") as file:
-        for i, col in enumerate(FLOAT_COLUMNS):
+        for i, col in enumerate(TRAIN_FLOAT):
             numerical_imputer = datawig.SimpleImputer(
-                input_columns=FLOAT_COLUMNS,  # column(s) containing information about the column we want to impute
+                input_columns=TRAIN_FLOAT,  # column(s) containing information about the column we want to impute
                 output_column=col,  # the column we'd like to impute values for
                 output_path=f"{col}_imputer_model",  # stores model data and metrics
             )
@@ -50,7 +97,7 @@ def main():
 
             male_mse = mse(male_df[f"{col}_imputed"], male_df[col])
             female_mse = mse(female_df[f"{col}_imputed"], female_df[col])
-            print(f"Finished column {i}/{len(FLOAT_COLUMNS)-1}")
+            print(f"Finished column {i}/{len(TRAIN_FLOAT)-1}")
             file.write(f"{col}:\nMale RMSE: {male_mse}\nFemale RMSE: {female_mse}\n")
 
 
--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 set PIP_EXTRA_INDEX_URL=https://pypi.python.org/simple https://download.pytorch.org/whl/cu117
 
-python -m venv venv
-./venv/Scripts/activate
+python3 -m venv venv
 
-pip install -e .
+set venv=.\venv\Scripts\activate
+%venv% && pip install --upgrade pip
+%venv% && pip install -e .
