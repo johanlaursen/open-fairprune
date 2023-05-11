d84e74e54aff5001e1c4fa1427d5485bc5774db6
christoffer <csom@itu.dk>
2023-05-11 10:26:20 +0200

    change NN architecture

--- src/open_fairprune/simple_nn.py
+++ src/open_fairprune/simple_nn.py
@@ -3,11 +3,17 @@ import torch.nn as nn
 
 from open_fairprune.data_util import INPUT_SIZE
 
-MODEL_NAME = "SimpleNNv2"
+MODEL_NAME = "SimpleNNv3"
 
 model = torch.nn.Sequential(
     nn.Linear(INPUT_SIZE, 1000),
     nn.ReLU(),
-    nn.Dropout(0.25),
-    nn.Linear(1000, 2),
+    nn.Dropout(0.20),
+    nn.Linear(1000, 100),
+    nn.ReLU(),
+    nn.Dropout(0.20),
+    nn.Linear(100, 10),
+    nn.ReLU(),
+    nn.Dropout(0.20),
+    nn.Linear(10, 2),
 )
