54710b45bb4c65332072aa6467894a0b985dc42c
christoffer <csom@itu.dk>
2023-05-13 12:18:33 +0200

    decrease model size to fit in memory2

--- src/open_fairprune/simple_nn.py
+++ src/open_fairprune/simple_nn.py
@@ -6,14 +6,14 @@ from open_fairprune.data_util import INPUT_SIZE
 MODEL_NAME = "SimpleNNv3"
 
 model = torch.nn.Sequential(
-    nn.Linear(INPUT_SIZE, 1000, bias=False),
+    nn.Linear(INPUT_SIZE, 500, bias=False),
     nn.ReLU(),
     nn.Dropout(0.50),
-    nn.Linear(1000, 1000, bias=False),
+    nn.Linear(500, 500, bias=False),
     nn.ReLU(),
     nn.Dropout(0.50),
-    nn.Linear(1000, 1000, bias=False),
+    nn.Linear(500, 500, bias=False),
     nn.ReLU(),
     nn.Dropout(0.50),
-    nn.Linear(1000, 2, bias=False),
+    nn.Linear(500, 2, bias=False),
 )
