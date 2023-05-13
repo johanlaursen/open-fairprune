dcd6f51cdd4bddc6d31ae59adbda745d47a0193e
christoffer <csom@itu.dk>
2023-05-13 11:47:37 +0200

    ensure consistent data

--- src/open_fairprune/data_util.py
+++ src/open_fairprune/data_util.py
@@ -10,9 +10,7 @@ import mlflow
 import numpy as np
 import pandas as pd
 import torch
-from click import FLOAT
 from sklearn.impute import SimpleImputer
-from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from torch.utils.data.dataloader import DataLoader, Dataset
 
@@ -23,7 +21,7 @@ import open_fairprune as this
 root = Path(this.__path__[0]).parents[1]
 DATA_PATH = root / "data"
 
-INPUT_SIZE = 204
+INPUT_SIZE = 186
 
 
 ORDINAL_COLUMNS = [
@@ -85,6 +83,13 @@ def timeit(msg: str) -> float:
     print(f"{start_date} Time: {msg} {time.perf_counter() - start:.3f} seconds")
 
 
+def filter_mlflow_data(**filters) -> pd.DataFrame:
+    runs = mlflow.search_runs()
+    for k, v in filters.items():
+        runs = runs[runs[k] == v]
+    return runs
+
+
 def load_model(id: str = "latest", model_name: str = "model", return_run_id=False):
     """E.g.: model = load_model("aa246d9d2106472492442ff362b1b143")"""
     if id == "latest":
@@ -125,108 +130,63 @@ def get_split_df(split="train"):
     return df
 
 
-class LoanDataset(Dataset):
-    def __init__(
-        self,
-        split: Literal["train"] | Literal["dev"] | Literal["test"],
-        returns=["data", "group", "label"],
-        transform=None,
-        *,
-        fuck_your_ram: int = 0,
-    ):
-        self.transform = transform
-        self.returns = returns
+def export_data():
+    train_df = get_split_df("train")
+    train_target = torch.tensor(train_df["T"].to_numpy(), dtype=torch.long)
+    train_group = torch.tensor(train_df.G.to_numpy(), dtype=torch.long)
+    train_df = train_df.drop(columns=["T", "G"])
 
-        df = get_split_df(split)
-        train_df = get_split_df("train")
-        # df.Default.value_counts()  # 0=80009, 1=7031, 11:1 ratio
-        # df.Client_Gender.value_counts()  # Male=56070, Female=29263, XNA=2, M:F ratio=2:1
+    # Dropping columns with more than 50% missing values (except score sources)
+    train_df = train_df.drop(["Own_House_Age", "Social_Circle_Default"], axis=1)
 
-        # gb = df.groupby("Default")
-        # g0, g1 = gb.get_group(0), gb.get_group(1)
-        # g1 = g1.sample(len(g0), replace=True)
-        # df = pd.concat([g0.iloc[:8000], g1])
+    # train imputers
+    ordinal = list(set(ORDINAL_COLUMNS) - {"Age_Days", "Own_House_Age", "Social_Circle_Default"})
+    number_imputer = SimpleImputer(strategy="median").set_output(transform="pandas").fit(train_df[ordinal])
 
-        self.target = torch.tensor(df["T"].to_numpy(), dtype=torch.long)
-        self.group = torch.tensor(df.G.to_numpy(), dtype=torch.long)
+    categorical = list(set(CATEGORICAL) - {"G", "T"})
+    category_imputer = SimpleImputer(strategy="most_frequent").set_output(transform="pandas").fit(train_df[categorical])
+    category_1hot = (
+        OneHotEncoder(sparse_output=False, handle_unknown="ignore", drop="first")
+        .set_output(transform="pandas")
+        .fit(category_imputer.transform(train_df[categorical]))
+    )
+
+    def impute(df):
+        return pd.concat(
+            [
+                number_imputer.transform(df[ordinal]),
+                category_1hot.transform(category_imputer.transform(df[categorical])),
+            ],
+            axis=1,
+        ).astype("float32")
+
+    train_df_1hot = impute(train_df)
+    scaler = StandardScaler().set_output(transform="pandas").fit(train_df_1hot)
+    scaled_train_df = scaler.transform(train_df_1hot)
+
+    dfs = [(scaled_train_df, train_target, train_group)]
+    for df in [get_split_df("dev"), get_split_df("test")]:
+        target = torch.tensor(df["T"].to_numpy(), dtype=torch.long)
+        group = torch.tensor(df.G.to_numpy(), dtype=torch.long)
+
+        df = df.drop(["Own_House_Age", "Social_Circle_Default"], axis=1)
         df = df.drop(columns=["T", "G"])
 
-        df["Accompany_Client"] = df["Accompany_Client"].replace("##", np.nan)
+        df_1hot = impute(df)
+        scaled_df = scaler.transform(df_1hot)
 
-        # Dropping columns with more than 50% missing values (except score sources)
-        df = df.drop(["Own_House_Age", "Social_Circle_Default"], axis=1)
+        assert len(scaled_df.columns) == INPUT_SIZE, f"{INPUT_SIZE = } != {len(scaled_df.columns) = }"
+        dfs.append((scaled_df, target, group))
 
-        # # We want a job category unknown, instead of the most common
-        # df["Client_Occupation"] = df["Client_Occupation"].fillna("Unknown")
-
-        def get_cat(df):
-            CAT = list(set(CATEGORICAL) - {"G", "T"})
-            return (
-                OneHotEncoder(sparse_output=False, handle_unknown="ignore", drop="first")
-                .set_output(transform="pandas")
-                .fit(train_df[CAT])
-                .transform(
-                    SimpleImputer(strategy="most_frequent")
-                    .set_output(transform="pandas")
-                    .fit(train_df[CAT])
-                    .transform(df[CAT])
-                )
-            )
-
-        def get_ordinal(df):
-            ordinal = list(set(ORDINAL_COLUMNS) - {"Age_Days", "Own_House_Age", "Social_Circle_Default"})
-            return (
-                SimpleImputer(strategy="median")
-                .set_output(transform="pandas")
-                .fit(train_df[ordinal])
-                .transform(df[ordinal])
-            )
-
-        df = pd.concat([get_cat(df), get_ordinal(df)], axis=1)
-        train_df = pd.concat([get_cat(train_df), get_ordinal(train_df)], axis=1)
-        df = StandardScaler().set_output(transform="pandas").fit(train_df).transform(df).astype("float32")
-
-        self.df = df
-
-        self.fuck_your_ram = fuck_your_ram  # Load this many samples into memory
-        self._gigacache = {}
-        assert (
-            len(self.df.columns) == INPUT_SIZE
-        ), f"Please update training data feature size: {INPUT_SIZE = } to {len(self.df.columns) = }"
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, idx):
-        if (cached := self._gigacache.get(idx)) is not None:
-            return cached
-
-        row = self.df.iloc[idx]
-
-        # Do callables, such that we do lazy loading
-        returns = {
-            "data": lambda row: row.values,
-            "group": lambda row: self.group[idx],
-            "label": lambda row: self.target[idx],
-        }
-
-        output = [returns[key](row) for key in self.returns]
-        if self.transform:
-            output = self.transform(output)
-        if idx < len(self._gigacache):
-            self._gigacache[idx] = output
-        return output
+    torch.save(scaled_train_df.columns, DATA_PATH / f"data.columns.pt")
+    for split, (data, target, group) in zip(["train", "dev", "test"], dfs):
+        torch.save(torch.from_numpy(data.to_numpy()), DATA_PATH / f"{split}_data.pt")
+        torch.save(group, DATA_PATH / f"{split}_group.pt")
+        torch.save(target, DATA_PATH / f"{split}_label.pt")
 
 
 def get_dataset(split: str, returns=["data", "group", "label"]):
-    dataset = LoanDataset(split, returns=returns)
-    loader = DataLoader(
-        dataset,
-        shuffle=True,
-        batch_size=len(dataset),
-        drop_last=True,
-    )
-    return next(iter(loader))
+    return [torch.load(open(DATA_PATH / f"{split}_{return_}.pt", "rb")) for return_ in returns]
 
 
 def get_git_hash():
@@ -238,7 +198,7 @@ def get_git_hash():
 
 
 if __name__ == "__main__":
-    with timeit("get data"):
+    with timeit("get_data"):
         features, group, labels = get_dataset("train")
 
     print(features.shape)
