29e5c156e4e17adaec4cfb6f315021871df2ef70
christoffer <csom@itu.dk>
2023-05-13 23:58:26 +0200

    update data exploration

--- src/open_fairprune/data_exploration.py
+++ src/open_fairprune/data_exploration.py
@@ -1,76 +1,60 @@
 import itertools
-from sre_parse import CATEGORIES
-from unicodedata import numeric
 
 import holoviews as hv
 import hvplot.pandas
-import numpy as np
 import pandas as pd
-from click import FLOAT
 
-from open_fairprune.data_util import (
-    CATEGORICAL,
-    FLOAT_COLUMNS,
-    get_dataset,
-    get_split_df,
-)
+from open_fairprune.data_util import CATEGORICAL, ORDINAL_COLUMNS, get_split_df
 
 hv.extension("bokeh")
 
-FLOAT_COLUMNS = list(set(FLOAT_COLUMNS) - {"Default", "Age_Days"} | {"G", "T"})
+ORDINAL_COLUMNS = list(set(ORDINAL_COLUMNS) - {"Default", "Age_Days"} | {"G", "T"})
 
-FILLNA = -0.2
 df = get_split_df("dev")
 
-minority_size = min(df["G"].value_counts())
-(_, df0), (_, df1) = df.groupby("G")
-df = pd.concat([df0.sample(minority_size), df1.sample(minority_size)])
-
-non_numeric_columns = list(set(df.columns) - set(FLOAT_COLUMNS))
+non_numeric_columns = list(set(df.columns) - set(ORDINAL_COLUMNS))
 cat_df = df[non_numeric_columns].astype("category")
 categorical_ids_df = pd.concat([series.cat.codes.rename(series.name) for _, series in cat_df.items()], axis=1)
 
-numeric_df = pd.concat([df[FLOAT_COLUMNS].astype(float), categorical_ids_df], axis=1).fillna(FILLNA)
-
-# plot = hvplot.scatter_matrix(df, c="G", alpha=0.1)
-# hv.save(plot, "correlation.html")
+numeric_df = pd.concat([df[ORDINAL_COLUMNS].astype(float), categorical_ids_df], axis=1)
 
 
-def get_correlation_scatters(columns=[c for c in numeric_df if "Source" in c]):
+def get_correlation_scatters(numeric_df, cols1, cols2):
     kwargs = {
         "width": 300,
         "height": 300,
-        "xlim": (FILLNA - 0.1, 1),
-        "ylim": (FILLNA - 0.1, 1),
+        "xlim": (-0.1, 1),
+        "ylim": (-0.1, 1),
         "legend_position": "bottom_left",
         "xticks": 1,
         "yticks": 1,
     }
     t_plots = []
     g_plots = []
-    df = numeric_df.assign(**{x: f"{x} = " + numeric_df[x].astype(int).astype(str) for x in ["G", "T"]})
-    for col1, col2 in itertools.combinations(columns, r=2):
+    df_full = numeric_df.assign(**{x: f"{x} = " + numeric_df[x].astype(int).astype(str) for x in ["G", "T"]})
+    for col1, col2 in itertools.product(cols1, cols2):
+        df = df_full.dropna(subset=[col1, col2])
         t_plots.append(df.hvplot.scatter(x=col1, y=col2, c="T").opts(**kwargs, alpha=0.3))
         g_plots.append(df.hvplot.scatter(x=col1, y=col2, c="G").opts(**kwargs, alpha=0.3))
-    plot = hv.Layout(t_plots + g_plots).cols(3)
+    plot = hv.Layout(t_plots + g_plots)
     plot
-    return plot
+    return plot.cols(len(plot) // 2)
 
 
-def get_group_conditional_prob_plots():
-    target = "G"
+def get_group_conditional_prob_plots(target="G"):
     densities = {}
-    for col in numeric_df[list(set(FLOAT_COLUMNS) - set(CATEGORICAL))]:
+    for col in numeric_df[list(set(ORDINAL_COLUMNS) - set(CATEGORICAL))]:
+        df = numeric_df.dropna(subset=[col])
         if col == target:
             continue
-        densities[col] = numeric_df.hvplot.kde(y=col, by=target, width=400, height=200, yaxis=None)
+        densities[col] = df.hvplot.kde(y=col, by=target, bandwidth=0.01, width=400, height=200)
 
-    for col in numeric_df[CATEGORICAL]:
+    for col in df[CATEGORICAL]:
         if col == target:
             continue
-        if numeric_df[col].nunique() > 25:
+        if df[col].nunique() > 25:
             continue
-        counts = numeric_df[[col, target]].value_counts()
+        counts = df[[col, target]].value_counts()
 
         densities[col] = (counts / counts.groupby(target).sum()).hvplot.bar(
             ylabel=f"Pr({col} | {target})", title="", width=400, height=200
@@ -79,14 +63,13 @@ def get_group_conditional_prob_plots():
     return densities
 
 
-nonlinear_correlations = get_correlation_scatters()
+nonlinear_correlations = get_correlation_scatters(numeric_df, ["House_Own"], ["Score_Source_1"])
+nonlinear_correlations
 
-densities = get_group_conditional_prob_plots()
+
+densities = get_group_conditional_prob_plots(target="G")
 plot = hv.Layout(densities.values()).cols(1)
-numeric_df
 
-
-# hv.save(hv.Layout(plots.values()).cols(1), "group_densities.html")
 (
     # densities["G"]
     densities["T"]
@@ -96,7 +79,7 @@ numeric_df
 
 (
     densities["Score_Source_3"]
-    + densities["Score_Source_2"]  # Not so big
+    + densities["Score_Source_2"].opts(xlim=(0, 1))  # Not so big
     + densities["Score_Source_1"]  # Group 1 has way more NaNs, yet higher scores on average
     + densities["Phone_Change"]
     + densities["Homephone_Tag"]
@@ -107,3 +90,10 @@ numeric_df
 
 # Proxies
 (densities["ID_Days"] + densities["Registration_Days"] + densities["Employed_Days"])
+
+plot = get_group_conditional_prob_plots(target="G")["Score_Source_1"].opts(
+    width=250, legend_position="top_left", ylabel="Pr(Score_Source_1 | G)"
+) + get_group_conditional_prob_plots(target="T")["Score_Source_1"].opts(
+    width=250, legend_position="top_right", ylabel="Pr(Score_Source_1 | T)"
+)
+plot
