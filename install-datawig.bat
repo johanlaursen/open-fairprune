eca217f09e7b613e1516f9c22efecc73c02791c1
christoffer <csom@itu.dk>
2023-05-17 14:25:27 +0200

    remove old install

--- install-datawig.bat
+++
@@ -1,7 +0,0 @@
-:: install visual studio to get a C compiler
-python3.8 -m venv venv
-
-set venv=.\venv\Scripts\activate
-%venv% && pip install --upgrade pip
-%venv% && pip install datawig
-%venv% && pip install Cython==0.29.34 numpy==1.17.3 pandas==1.3.5
